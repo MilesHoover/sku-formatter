b90e0e0416ed9ec4a731ab537a66d0aa869810b6
miles <mhoover709@gmail.com>
2026-08-27 19:32:27 -0400

    writing read file code

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-# open the file for reading
-file = open("input.txt", "r")
-print(file.read())
+import re
 
+# open the file for reading
 # iterate through file to find skus
 # skus are determined by an unbroken string of numbers separated by a space or a common or both like: "12345678, 123456789 987654321"
 # I will use regex to parse out skus
-# if I line has no numbers, go to the next line to keep checking until end of file
-
+# if a line has no numbers, go to the next line to keep checking until end of file
 # format them and add them to a list_one
 # strip any commas, padded zeros (like 0012345678), unnecessary spaces
-# confirming this is what I want
+list_one = []
 
-# close the file
+with open("input.txt", "r") as file:
+    for line in file:
+        x = re.findall(r"\d*", line) # "\d" means decimal digits and "*" means 0 or more times
+        for sku in x:
+            print(re.findall(r"\A0*", sku)) # "\A" means matching any 0s                    #check re.sub and ^ tomorrow to kick out leading zeros before adding them to the list_one
+            list_one.append(sku)
+
+print("\nlist_one:")    
+print(list_one)
 
 # dedupe the list into a set
 # iterate through the list adding a new sku to a set, for every sku in the original list, check it against what's been added to the new, deduped set. If it already exits, its a dupe and doesn't need to be added. 
