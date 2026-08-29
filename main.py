fb2d5636eab9382127b6718a57d60a96c1d07a54
miles <mhoover709@gmail.com>
2026-08-29 13:35:32 -0400

    complete base logic

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-import re
+import argparse, re
 
-def extract_skus():
+def get_file_name():
+    parser = argparse.ArgumentParser(prog="column-formatter", description="A program to format SKUs")
+    parser.add_argument('file_name', help="The name of the text file containing skus to format")
+    args = parser.parse_args()
+    file_name = args.file_name
+
+    return file_name
+
+def extract_skus(file_name):
     raw_sku_list = []
 
-    with open("input.txt", "r") as file:
+    with open(file_name, "r") as file:
         for line in file:
             raw_skus = re.findall(r"\d+", line) # "\d" means decimal digits and "+" means 1 or more times
             for sku in raw_skus:
@@ -29,16 +37,18 @@ def format_skus(raw_sku_list):
 
     return deduped_sku_list
 
-def write_skus(deduped_sku_list):
-    with open("input.txt", "w") as file:
+def write_skus(deduped_sku_list, file_name):
+    with open(file_name, "w") as file:
         for index, sku in enumerate(deduped_sku_list):
             formatted_sku = (f"'{sku}'")
             if index < len(deduped_sku_list) - 1:
                 file.write(f"{formatted_sku},\n")
             else:
                 file.write(formatted_sku)
+    print("Format complete")
 
 if __name__ == "__main__":
-    raw = extract_skus()
+    file_name = get_file_name()
+    raw = extract_skus(file_name)
     formatted = format_skus(raw)
-    write_skus(formatted)
+    write_skus(formatted, file_name)
